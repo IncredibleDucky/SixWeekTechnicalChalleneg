cc0ec83c70fb1a5a86d0e00cbf5529779a9ea873
Matt Bailey <incredibleducky@gmail.com>
2015-08-12 08:02:56 -0700

    Initial Commit

---
+++ Six Week Technical Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Six Week Technical Challenge
+//
+//  Created by Mattthew Bailey on 8/12/15.
+//  Copyright (c) 2015 Mattthew Bailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Six Week Technical ChallengeTests/Six_Week_Technical_ChallengeTests.m
@@ -0,0 +1,40 @@
+//
+//  Six_Week_Technical_ChallengeTests.m
+//  Six Week Technical ChallengeTests
+//
+//  Created by Mattthew Bailey on 8/12/15.
+//  Copyright (c) 2015 Mattthew Bailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Six_Week_Technical_ChallengeTests : XCTestCase
+
+@end
+
+@implementation Six_Week_Technical_ChallengeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
